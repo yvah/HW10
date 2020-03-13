a985d7ca99b08de236faf87466306a8e09d1e753
bonnybelle <55929630+bonnybelle@users.noreply.github.com>
2020-03-13 18:13:26 +0200

    Update task4.py

--- task4.py
+++ task4.py
@@ -6,7 +6,7 @@ result = open('t4result.txt', 'w')
 with io.open('C:/Users/bonny/PycharmProjects/FirstPrj/advanced/HW10/t4file.txt', encoding='utf-8') as file:
     for line in file:
         counter = 0
-        list_line = [str(i) for i in line.split()]
+        list_line = [i for i in line.split()]
         for word in list_line:
             if 3 <= len(word) <= 5:
                 counter += 1
@@ -18,6 +18,6 @@ with io.open('C:/Users/bonny/PycharmProjects/FirstPrj/advanced/HW10/t4file.txt',
             if 3 <= len(word) <= 5 and del_counter < counter:
                 del_counter += 1
                 continue
-            new_line.append(f'{word} ')
-        result.write(''.join(new_line) + '\n')
+            new_line.append(f'{word}')
+        result.write(' '.join(new_line) + '\n')
 result.close()
